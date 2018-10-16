4a65940f9e54875c20a2a2c2ca6a5cf6a6c40502
Darja Ryazhskikh <darja.ryazhskikh@gmail.com>
2018-10-17 00:14:28 +0300

    Removed DaoWithUpsert

--- app/src/main/java/com/darja/moviedb/db/dao/GenreDao.kt
+++ app/src/main/java/com/darja/moviedb/db/dao/GenreDao.kt
@@ -1,22 +1,39 @@
 package com.darja.moviedb.db.dao
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import com.darja.moviedb.db.model.Genre
+import com.darja.moviedb.util.DPLog
 
 @Dao
-abstract class GenreDao: DaoWithUpsert<Genre>() {
-    @Query("select * from genres")
-    abstract fun select(): Array<Genre>
+interface GenreDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(item: Genre): Long
 
-    @Query("select * from genres where genreId in(:ids)")
-    abstract fun select(ids: Array<Int>): Array<Genre>
+    @Update
+    fun update(item: Genre)
 
     @Query("select `rowId` from genres where genreId = :genreId")
-    abstract fun select(genreId: Int): Long
+    fun select(genreId: Int): Long
+
+    @Query("select * from genres")
+    fun select(): Array<Genre>
+
+    @Query("select * from genres where genreId in(:ids)")
+    fun select(ids: Array<Int>): Array<Genre>
 
     @Query("delete from genres")
-    abstract fun clear()
+    fun clear()
 
-    override fun select(item: Genre) = select(item.genreId)
+    @Transaction
+    fun upsert(item: Genre): Long {
+        var id = insert(item)
+        if (id < 0) {
+            update(item)
+            id = select(item.genreId)
+            DPLog.v("Updated %s: %s", id, item)
+        } else {
+            DPLog.v("Inserted %s: %s", id, item)
+        }
+        return id
+    }
 }
