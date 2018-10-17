242dbb4e5c1f0a4782b01b5a456d610b51147328
Darja Ryazhskikh <darja.ryazhskikh@gmail.com>
2018-10-17 12:28:28 +0300

    Logs are disabled for release version

--- app/src/main/java/com/darja/moviedb/MovieDbApp.kt
+++ app/src/main/java/com/darja/moviedb/MovieDbApp.kt
@@ -2,8 +2,11 @@ package com.darja.moviedb
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.content.pm.ApplicationInfo
 import com.darja.moviedb.di.AppModule
 import com.darja.moviedb.di.DaggerAppComponent
+import com.darja.moviedb.util.DPLog
 import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
@@ -16,6 +19,11 @@ class MovieDbApp @Inject constructor() : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (!isDebuggable(this)) {
+            DPLog.disable()
+        }
+
         initInjector()
         Fresco.initialize(this)
     }
@@ -28,5 +36,9 @@ class MovieDbApp @Inject constructor() : Application(), HasActivityInjector {
             .appModule(AppModule(this))
             .build()
             .inject(this)
+    }
+
+    private fun isDebuggable(context: Context): Boolean {
+        return 0 != context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
     }
 }
